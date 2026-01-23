163d1a17caaa9a879ca72a0dd2043f68dcf6a4e0
cometscome <cometscome@gmail.com>
2026-01-23 16:36:06 +0900

    AD bug fix

--- src/LinearAlgebras/linearalgebra_D.jl
+++ src/LinearAlgebras/linearalgebra_D.jl
@@ -5114,3 +5114,22 @@ function kernel_4d_Traceless_antihermitian_add!(i, c, vin, factor, dindexer, ::V
         sr3i * (imag(y11) + imag(y22) - 2 * imag(y33)) * factor +
         c[8, 1, indices...]
 end
+
+#dot(A,B) = tr(A^+*B)
+function LinearAlgebra.dot(A::LatticeMatrix{D,T1,AT1,NC1,NG,nw}, B::LatticeMatrix{D,T2,AT2,NC1,NG,nw}) where {D,NG,T1,T2,AT1,AT2,NC1,nw}
+    s = JACC.parallel_reduce(prod(A.PN), kernel_dot_D,
+        A.A, B.A, A.indexer, Val(NC1), Val(NG), Val(nw); init=zero(eltype(A.A)), op=+)
+    s = MPI.Allreduce(s, MPI.SUM, A.comm)
+end
+
+@inline function kernel_dot_D(i, A, B, dindexer, ::Val{NC1}, ::Val{NG}, ::Val{nw}) where {NC1,nw,NG}
+    indices = delinearize(dindexer, i, nw)
+    s = zero(eltype(A))
+
+    @inbounds for ialpha = 1:NG
+        for ic = 1:NC1
+            s += conj(A[ic, ialpha, indices...]) * B[ic, ialpha, indices...]
+        end
+    end
+    return s
+end
